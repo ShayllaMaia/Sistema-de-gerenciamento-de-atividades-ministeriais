43f3b3acad2639b5f9d89c81669c14f7e13588c3
emillyvsl <emilly.vitoria@sou.ufac.br>
2024-02-06 15:31:59 -0500

    feat: inclui rota "login"

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [{ path: 'cadastro', component: CadastroComponent }];
+const routes: Routes = [{ path: 'cadastro', component: CadastroComponent },
+{path:'login',component: LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent
+    CadastroComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
