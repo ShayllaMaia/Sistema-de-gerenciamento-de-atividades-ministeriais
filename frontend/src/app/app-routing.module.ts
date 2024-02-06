70a6fd60c8d28d26732403feeade200831e1f79e
emillyvsl <emilly.vitoria@sou.ufac.br>
2024-02-06 15:24:00 -0500

    feat: inclui rota "cadastro"

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'cadastro', component: CadastroComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
