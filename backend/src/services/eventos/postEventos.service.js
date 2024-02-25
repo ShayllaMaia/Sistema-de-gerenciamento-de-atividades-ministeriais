b7af5a981796504cf215b2565875ae96d5047c27
sthefanySF <sthefanysantos0815@gmail.com>
2024-02-25 13:04:10 -0500

    feat: integra listar eventos parcial

--- backend/src/services/eventos/postEventos.service.js
+++ backend/src/services/eventos/postEventos.service.js
@@ -9,8 +9,9 @@ const postEventosService = async (dados) => {
 
         // Convertendo a string de hora para um objeto Date
         const dataFormatada = new Date(data).toISOString();
-        const horaInicioObj = new Date(`1970-01-01T${horaInicio}`);
-        const horaFimObj = new Date(`1970-01-01T${horaFim}`);
+        const horaInicioObj = new Date(`${data}T${horaInicio}`);
+        const horaFimObj = new Date(`${data}T${horaFim}`);
+    
 
         if (horaInicioObj >= horaFimObj) {
             throw new AppError('A hora de início deve ser anterior à hora de término.', 400);
--- frontend/src/app/components/evento-listar/evento-listar.component.ts
+++ frontend/src/app/components/evento-listar/evento-listar.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventoService } from 'src/app/services/evento.service';
+import { EventoInterface } from 'src/app/model/evento.interface';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-evento-listar',
   templateUrl: './evento-listar.component.html',
   styleUrls: ['./evento-listar.component.css']
 })
-export class EventoListarComponent {
+export class EventoListarComponent implements OnInit {
+  registro: EventoInterface[] = [];
+  constructor(private eventoService: EventoService, private router: Router) {}
 
+
+  ngOnInit(): void {
+    this.carregarEventos();
+  }
+
+  carregarEventos(): void {
+    this.eventoService.getEventos().subscribe(
+      (eventos) => {
+        console.log('Eventos carregados:', eventos);
+        this.registro = eventos;
+      },
+      (error) => {
+        console.error('Erro ao carregar eventos:', error);
+      }
+
+  
+    );
+  }
+
+  editarEvento(evento: EventoInterface): void {
+    // Implementar a navegação para a página de edição com o ID do evento
+    this.router.navigate(['/editar-evento', evento.id]);
+  }
+
+  excluirEvento(eventoId: string): void {
+    // Implementar a lógica para excluir o evento
+    // Chame o método do serviço para excluir e, em seguida, recarregue a lista
+    this.eventoService.excluirEvento(eventoId).subscribe(
+      () => {
+        console.log('Evento excluído com sucesso');
+        this.carregarEventos();
+      },
+      (error: any) => {
+        console.error('Erro ao carregar eventos:', error);
+      }
+    );
+  }
+  navegarParaCadastro(): void {
+    // Implementar a navegação para a página de cadastro
+    this.router.navigate(['/cadastro-evento']);
+  }
 }
