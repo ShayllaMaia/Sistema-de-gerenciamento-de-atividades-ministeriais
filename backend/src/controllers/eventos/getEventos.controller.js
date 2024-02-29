32afb65a59336cbc58057e1ab19f56be37636b38
sthefanySF <sthefanysantos0815@gmail.com>
2024-02-29 18:55:41 -0500

    fix: ajuste em getEventos.controller

--- backend/src/controllers/eventos/getEventos.controller.js
+++ backend/src/controllers/eventos/getEventos.controller.js
@@ -1,10 +1,10 @@
 import { getEventosService } from "../../services/eventos/getEventos.service.js";
 
-const getEventosController = async(req,res ) => {
-    const eventos = await getEventosService();
+const getEventosController = async(req,res) => {
     const authHeader = req.headers["authorization"];
+    const eventos = await getEventosService(authHeader);
 
-    return res.status(200).json(eventos,authHeader);
+    return res.status(200).json(eventos);
 }
 
-export { getEventosController};
+export { getEventosController}
