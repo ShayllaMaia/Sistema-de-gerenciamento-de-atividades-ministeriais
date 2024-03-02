2be21a2707196b2f805c3e66363b262421f4e1bc
saracf <saracunhha1@gmail.com>
2024-03-02 14:56:18 -0500

    fix: enviar o status http para o front dar refresh na pagina

--- backend/src/controllers/atividade/deleteAtividade.controller.js
+++ backend/src/controllers/atividade/deleteAtividade.controller.js
@@ -1,11 +1,11 @@
 import { deleteAtividadeService } from "../../services/atividade/deleteAtividade.service.js";
 
 const deletarAtividadeController = async (req, res) => {
-  const { id } = req.params;
-  const authHeader = req.headers["authorization"];
- 
-  await deleteAtividadeService(id, authHeader);
+    const { id } = req.params;
+    const authHeader = req.headers["authorization"];
 
-  return res.status(204);
+    await deleteAtividadeService(id, authHeader);
+
+    return res.sendStatus(204);
 };
 export { deletarAtividadeController };
