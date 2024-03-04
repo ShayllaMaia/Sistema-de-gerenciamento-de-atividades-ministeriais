0cc3c2c16e4f41fa8d3a129c0a538a45a6e6ce83
Emilly Silva <115952368+emillyvsl@users.noreply.github.com>
2024-03-04 00:51:56 -0500

    Update environment.development.ts

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    API_URL: 'https://sistema-de-gerenciamento-de-atividades.onrender.com/',
+    API_URL: 'https://sistema-de-gerenciamento-de-atividades.onrender.com',
   };
