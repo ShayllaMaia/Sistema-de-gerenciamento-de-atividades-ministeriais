ba061520dc3fa40532bb03dcd4cc7edc98d6ea6a
idimitrisc47 <dimitrisc47@gmail.com>
2024-09-02 19:35:20 -0500

    Ministerio lider

--- backend/src/controllers/ministerioLider/retrieveMinisterioLider.controller.js
+++ backend/src/controllers/ministerioLider/retrieveMinisterioLider.controller.js
@@ -1,15 +1,18 @@
 import { retrieveMinisterioLiderService } from "../../services/ministerioLider/retrieveMinisterioLider.service.js";
 
 const retrieveMinisterioLiderController = async (req, res) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const ministeriosLiderados = await getMinisterioLiderService(token);
-        res.json(ministeriosLiderados);
-    } catch (error) {
-        console.error('Erro ao obter ministérios liderados:', error.message);
-        res.status(500).send(`Error: ${error.message}`);
-    }
+  try {
+    const authHeader = req.headers["authorization"];
+    const { id } = req.params;
+    const ministeriosLiderados = await retrieveMinisterioLiderService(
+      id,
+      authHeader
+    );
+    res.json(ministeriosLiderados);
+  } catch (error) {
+    console.error("Erro ao obter ministérios liderados:", error.message);
+    res.status(500).send(`Error: ${error.message}`);
+  }
 };
-
 
 export { retrieveMinisterioLiderController };
--- backend/src/services/ministerioLider/getministerioLider.service.js
+++ backend/src/services/ministerioLider/getministerioLider.service.js
@@ -3,37 +3,40 @@ import { retornaInfoToken } from "../../../middlewares/retornaInfoToen.middliwar
 
 const prisma = new PrismaClient();
 const getMinisterioLiderService = async (token) => {
-    try {
-        const liderId = await retornaInfoToken(token);
-        console.log('Lider ID:', liderId); // Verifique o ID do líder
+  try {
+    const liderId = await retornaInfoToken(token);
+    console.log("Lider ID:", liderId); // Verifique o ID do líder
 
-        const ministeriosLiderados = await prisma.ministerioLider.findMany({
-            where: {
-                lider_id: liderId,
-            },
-            include: {
-                ministerio: true, // Inclui as informações do ministério
-            },
-        });
+    const ministeriosLiderados = await prisma.ministerioLider.findMany({
+      where: {
+        lider_id: liderId,
+      },
+      include: {
+        ministerio: true,
+        lider: {
+          select: {
+            nome: true,
+          }
+        } // Inclui as informações do ministério
+      },
+    });
 
-        console.log('Consulta aos ministérios liderados:', ministeriosLiderados);
+    console.log("Consulta aos ministérios liderados:", ministeriosLiderados);
 
-        if (ministeriosLiderados.length === 0) {
-            console.log('Nenhum ministério encontrado para o líder:', liderId);
-            throw new Error('Ministério não encontrado!');
-        }
-
-        return ministeriosLiderados.map(ml => ({
-            ministerio_id: ml.ministerio.id,
-            lider_id: ml.lider_id,
-            ministerio: ml.ministerio
-        }));
-    } catch (error) {
-        console.error('Erro no serviço:', error.message);
-        throw error;
+    if (ministeriosLiderados.length === 0) {
+      console.log("Nenhum ministério encontrado para o líder:", liderId);
+      throw new Error("Ministério não encontrado!");
     }
+
+    return ministeriosLiderados.map((ml) => ({
+      ministerio_id: ml.ministerio.id,
+      lider_id: ml.lider_id,
+      ministerio: ml.ministerio,
+    }));
+  } catch (error) {
+    console.error("Erro no serviço:", error.message);
+    throw error;
+  }
 };
-
-
 
 export { getMinisterioLiderService };
